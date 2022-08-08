4d3215eaea50265e709c5cc70ee5cc658521dd51
Matthew Goebel <96901281+mattgoebs@users.noreply.github.com>
2022-08-07 20:19:34 -0400

    bank_account.py

--- d2/Bank Account/bank_Account.py
+++ d2/Bank Account/bank_Account.py
@@ -1,33 +1,3 @@
-# class BankAccount:
-#     # don't forget to add some default values for these parameters!
-#     def __init__(self, int_rate, balance): 
-#         self.balance = 0
-#         self.int_rate = 1.10
-#         # your code here! (remember, instance attributes go here)
-#         # don't worry about user info here; we'll involve the User class soon
-#     def deposit(self, amount):
-#         amount = int(input('Enter amount:'))
-#         self.balance += amount
-#         print(amount)
-#     def withdraw(self, amount):
-#         if(amount>self.balance):
-#             print('Insufficient funds: Charging a $5 fee')
-#             self.balance -= 5
-#         else:
-#             self.balance -= amount
-#             print(amount)
-#     def display_account_info(self):
-#         print(self.amount)
-#     def yield_interest(self):
-#         if(self.balance > 0):
-#             self.balance *= self.int_rate
-#             print(self.balance)
-
-
-
-
-
-
 class Bank_Account:
     def __init__(self):
         self.balance = 0
